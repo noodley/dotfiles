14560c4a56e3db34d463e44b42d9eaef00d6cb7a
Doug Weimer <dweimer@gmail.com>
2023-08-11 12:16:37 -0700

    nix: Cleanup removing unused home features and global host configs

--- nix/home/dougw/features/games/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }: {
-  imports = [
-    ./lutris.nix
-    ./steam.nix
-    ./prism-launcher.nix
-    ./runescape.nix
-  ];
-  home.packages = with pkgs; [ gamescope ];
-}
--- nix/hosts/common/global/default.nix
+++ nix/hosts/common/global/default.nix
@@ -2,19 +2,12 @@
 { inputs, outputs, pkgs, ... }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
-    #./acme.nix
-    #./auto-upgrade.nix
-    #./fish.nix
     ./locale.nix
     ./nix.nix
     #./openssh.nix
     ./optin-persistence.nix
-    #./podman.nix
     #./sops.nix
-    #./ssh-serve-store.nix
-    #./steam-hardware.nix
     ./systemd-initrd.nix
-    #./tailscale.nix
   ] ++ (builtins.attrValues outputs.nixosModules);
 
   
