ef3bb11445e13b1dba1aa6368e2bd9cb5f3109d2
Doug Weimer <dweimer@gmail.com>
2023-08-12 06:26:38 -0700

    nix-index: Add nix-index support and a persitent index

--- nix/home/dougw/features/cli/default.nix
+++ nix/home/dougw/features/cli/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./bash.nix
     ./git.nix
+    ./nix-index.nix
     ./ssh.nix
     ./tmux.nix
   ];
---
+++ nix/home/dougw/features/cli/nix-index.nix
@@ -0,0 +1,43 @@
+{ pkgs, config, ... }:
+let
+  update-script = pkgs.writeShellApplication {
+    name = "fetch-nix-index-database";
+    runtimeInputs = with pkgs; [ wget coreutils ];
+    text = ''
+      filename="index-x86_64-linux"
+      mkdir -p ~/.cache/nix-index
+      cd ~/.cache/nix-index
+      wget -N "https://github.com/Mic92/nix-index-database/releases/latest/download/$filename"
+      ln -f "$filename" files
+    '';
+  };
+in
+{
+  programs.nix-index.enable = true;
+
+  home.persistence = {
+    "/persist/root/home/${config.home.username}" = {
+      directories = [
+        ".cache/nix-index"
+      ];
+    };
+  };
+
+  systemd.user.services.nix-index-database-sync = {
+    Unit = { Description = "fetch mic92/nix-index-database"; };
+    Service = {
+      Type = "oneshot";
+      ExecStart = "${update-script}/bin/fetch-nix-index-database";
+      Restart = "on-failure";
+      RestartSec = "5m";
+    };
+  };
+  systemd.user.timers.nix-index-database-sync = {
+    Unit = { Description = "Automatic github:mic92/nix-index-database fetching"; };
+    Timer = {
+      OnBootSec = "10m";
+      OnUnitActiveSec = "24h";
+    };
+    Install = { WantedBy = [ "timers.target" ]; };
+  };
+}
