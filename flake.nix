194cc277d6750216147abf7b3037d7d885a6bfb9
Doug Weimer <dweimer@gmail.com>
2023-08-23 09:06:21 -0700

    nixos: Moving to unstable

--- flake.nix
+++ flake.nix
@@ -3,13 +3,14 @@
 
   inputs = {
     # NixPkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    #nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # NixPkgs Unstable (nixos-unstable)
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # Home Manager (release-22.05)
-    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    # Home Manager 
+    home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
     # Hardware Configuration
